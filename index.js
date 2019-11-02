8d4fafcabddc6d73045803734d4dc2a00595734e
Trubetskaia Inna <trubetskaya_inna@ukr.net>
2019-11-02 15:38:35 +0200

    added myMap function

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ someArr = [1, 0, 3, 10]
 someArr.myForEach(function (element, index, arr) {
   console.log(element, index, arr)
 })
+
+Array.prototype.myMap = function (fun) {
+  let arrResult = []
+  for (let i = 0; i < this.length; i++) {
+    arrResult.push(fun(this[i], i, this))
+  }
+  return arrResult
+}
+
+const arrTest = someArr.myMap(element => element * 2);
+console.log(arrTest)
